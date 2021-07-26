af66dfa8c6a714a18c6d5b83f99a13c1319762ce
Alejosebasp <asalejop@unal.edu.co>
2021-07-26 00:29:22 +0000

    Deploying to gh-pages from  @ 04eb47a98322b7ac1d694518d7b4888410ddfbda 🚀

---
+++ docs/sketches/image.js
@@ -0,0 +1,18 @@
+var quadrille;
+var image;
+
+function preload() {
+  image = loadImage('/visual/docs/sketches/images/mosaico/gato_mosaico.jpg');
+}
+
+function setup() {
+  createCanvas(800, 360);
+}
+
+function draw() {
+  if (frameCount % 200 === 0) {
+    let scl = 2 ** int(random(4));
+    quadrille = createQuadrille(20 * scl, image);
+    drawQuadrille(quadrille, 0, 0, 40 / scl, 1.6 / scl, color(random(255)));
+  }
+}
