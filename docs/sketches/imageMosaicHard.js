a6b3c71e400651323f11c7fc09ebf780561430ff
Alejosebasp <asalejop@unal.edu.co>
2021-07-26 02:10:46 +0000

    Deploying to gh-pages from  @ 46402c85c6267770e9f25ed5d43065102b579711 🚀

--- docs/sketches/imageMosaicHard.js
+++ docs/sketches/imageMosaicHard.js
@@ -1,9 +1,13 @@
-/*
-* Código tomado y adaptado de: https://editor.p5js.org/leftlife/sketches/cFD-TFQqe
-*/
+var quadrille;
+var image;
 
 function preload() {
-    img000 = loadImage("/visual/docs/sketches/images/000.jfif");
+
+  //imagen a convertir en mosaico
+  image = loadImage('/visual/docs/sketches/images/mosaico/gato_mosaico.jpg');
+
+  //Imagenes para el mosaico
+  img000 = loadImage("/visual/docs/sketches/images/000.jfif");
     img001 = loadImage("/visual/docs/sketches/images/001.jfif");
     img002 = loadImage("/visual/docs/sketches/images/002.jfif");
     img003 = loadImage("/visual/docs/sketches/images/003.jfif");
@@ -130,69 +134,51 @@ function preload() {
     img444 = loadImage("/visual/docs/sketches/images/444.jfif");
 }
 
+//Función para mapear un color de 0-255 entre 0-4 
 function mapColor(color) {
-    if (color <= 50) {
-        return 0;
-    }
-    else if (color <= 102) {
-        return 1;
-    }
-    else if (color <= 153) {
-        return 2;
-    }
-    else if (color <= 204) {
-        return 3;
-    }
-    else if (color <= 255) {
-        return 4;
-    }
+  if (color <= 50) {
+      return 0;
+  }
+  else if (color <= 102) {
+      return 1;
+  }
+  else if (color <= 153) {
+      return 2;
+  }
+  else if (color <= 204) {
+      return 3;
+  }
+  else if (color <= 255) {
+      return 4;
+  }
 }
 
-var video;
-var vScale = 5;
-var arregloImagenes;
-
 function setup() {
-    arregloImagenes = [{ indice: "000", imagen: img000 }, { indice: "001", imagen: img001 }, { indice: "002", imagen: img002 }, { indice: "003", imagen: img003 }, { indice: "004", imagen: img004 }, { indice: "010", imagen: img010 }, { indice: "011", imagen: img011 }, { indice: "012", imagen: img012 }, { indice: "013", imagen: img013 }, { indice: "014", imagen: img014 }, { indice: "020", imagen: img020 }, { indice: "021", imagen: img021 }, { indice: "022", imagen: img022 }, { indice: "023", imagen: img023 }, { indice: "024", imagen: img024 }, { indice: "030", imagen: img030 }, { indice: "031", imagen: img031 }, { indice: "032", imagen: img032 }, { indice: "033", imagen: img033 }, { indice: "034", imagen: img034 }, { indice: "040", imagen: img040 }, { indice: "041", imagen: img041 }, { indice: "042", imagen: img042 }, { indice: "043", imagen: img043 }, { indice: "044", imagen: img044 }, { indice: "100", imagen: img100 }, { indice: "101", imagen: img101 }, { indice: "102", imagen: img102 }, { indice: "103", imagen: img103 }, { indice: "104", imagen: img104 }, { indice: "110", imagen: img110 }, { indice: "111", imagen: img111 }, { indice: "112", imagen: img112 }, { indice: "113", imagen: img113 }, { indice: "114", imagen: img114 }, { indice: "120", imagen: img120 }, { indice: "121", imagen: img121 }, { indice: "122", imagen: img122 }, { indice: "123", imagen: img123 }, { indice: "124", imagen: img124 }, { indice: "130", imagen: img130 }, { indice: "131", imagen: img131 }, { indice: "132", imagen: img132 }, { indice: "133", imagen: img133 }, { indice: "134", imagen: img134 }, { indice: "140", imagen: img140 }, { indice: "141", imagen: img141 }, { indice: "142", imagen: img142 }, { indice: "143", imagen: img143 }, { indice: "144", imagen: img144 }, { indice: "200", imagen: img200 }, { indice: "201", imagen: img201 }, { indice: "202", imagen: img202 }, { indice: "203", imagen: img203 }, { indice: "204", imagen: img204 }, { indice: "210", imagen: img210 }, { indice: "211", imagen: img211 }, { indice: "212", imagen: img212 }, { indice: "213", imagen: img213 }, { indice: "214", imagen: img214 }, { indice: "220", imagen: img220 }, { indice: "221", imagen: img221 }, { indice: "222", imagen: img222 }, { indice: "223", imagen: img223 }, { indice: "224", imagen: img224 }, { indice: "230", imagen: img230 }, { indice: "231", imagen: img231 }, { indice: "232", imagen: img232 }, { indice: "233", imagen: img233 }, { indice: "234", imagen: img234 }, { indice: "240", imagen: img240 }, { indice: "241", imagen: img241 }, { indice: "242", imagen: img242 }, { indice: "243", imagen: img243 }, { indice: "244", imagen: img244 }, { indice: "300", imagen: img300 }, { indice: "301", imagen: img301 }, { indice: "302", imagen: img302 }, { indice: "303", imagen: img303 }, { indice: "304", imagen: img304 }, { indice: "310", imagen: img310 }, { indice: "311", imagen: img311 }, { indice: "312", imagen: img312 }, { indice: "313", imagen: img313 }, { indice: "314", imagen: img314 }, { indice: "320", imagen: img320 }, { indice: "321", imagen: img321 }, { indice: "322", imagen: img322 }, { indice: "323", imagen: img323 }, { indice: "324", imagen: img324 }, { indice: "330", imagen: img330 }, { indice: "331", imagen: img331 }, { indice: "332", imagen: img332 }, { indice: "333", imagen: img333 }, { indice: "334", imagen: img334 }, { indice: "340", imagen: img340 }, { indice: "341", imagen: img341 }, { indice: "342", imagen: img342 }, { indice: "343", imagen: img343 }, { indice: "344", imagen: img344 }, { indice: "400", imagen: img400 }, { indice: "401", imagen: img401 }, { indice: "402", imagen: img402 }, { indice: "403", imagen: img403 }, { indice: "404", imagen: img404 }, { indice: "410", imagen: img410 }, { indice: "411", imagen: img411 }, { indice: "412", imagen: img412 }, { indice: "413", imagen: img413 }, { indice: "414", imagen: img414 }, { indice: "420", imagen: img420 }, { indice: "421", imagen: img421 }, { indice: "422", imagen: img422 }, { indice: "423", imagen: img423 }, { indice: "424", imagen: img424 }, { indice: "430", imagen: img430 }, { indice: "431", imagen: img431 }, { indice: "432", imagen: img432 }, { indice: "433", imagen: img433 }, { indice: "434", imagen: img434 }, { indice: "440", imagen: img440 }, { indice: "441", imagen: img441 }, { indice: "442", imagen: img442 }, { indice: "443", imagen: img443 }, { indice: "444", imagen: img444 }]
-    createCanvas(640, 480);
-    pixelDensity(3);
-    frameRate(15);
-    video = createCapture(VIDEO);
-    video.size(width / vScale, height / vScale);
+  createCanvas(600, 650);
+  arregloImagenes = [{ indice: "000", imagen: img000 }, { indice: "001", imagen: img001 }, { indice: "002", imagen: img002 }, { indice: "003", imagen: img003 }, { indice: "004", imagen: img004 }, { indice: "010", imagen: img010 }, { indice: "011", imagen: img011 }, { indice: "012", imagen: img012 }, { indice: "013", imagen: img013 }, { indice: "014", imagen: img014 }, { indice: "020", imagen: img020 }, { indice: "021", imagen: img021 }, { indice: "022", imagen: img022 }, { indice: "023", imagen: img023 }, { indice: "024", imagen: img024 }, { indice: "030", imagen: img030 }, { indice: "031", imagen: img031 }, { indice: "032", imagen: img032 }, { indice: "033", imagen: img033 }, { indice: "034", imagen: img034 }, { indice: "040", imagen: img040 }, { indice: "041", imagen: img041 }, { indice: "042", imagen: img042 }, { indice: "043", imagen: img043 }, { indice: "044", imagen: img044 }, { indice: "100", imagen: img100 }, { indice: "101", imagen: img101 }, { indice: "102", imagen: img102 }, { indice: "103", imagen: img103 }, { indice: "104", imagen: img104 }, { indice: "110", imagen: img110 }, { indice: "111", imagen: img111 }, { indice: "112", imagen: img112 }, { indice: "113", imagen: img113 }, { indice: "114", imagen: img114 }, { indice: "120", imagen: img120 }, { indice: "121", imagen: img121 }, { indice: "122", imagen: img122 }, { indice: "123", imagen: img123 }, { indice: "124", imagen: img124 }, { indice: "130", imagen: img130 }, { indice: "131", imagen: img131 }, { indice: "132", imagen: img132 }, { indice: "133", imagen: img133 }, { indice: "134", imagen: img134 }, { indice: "140", imagen: img140 }, { indice: "141", imagen: img141 }, { indice: "142", imagen: img142 }, { indice: "143", imagen: img143 }, { indice: "144", imagen: img144 }, { indice: "200", imagen: img200 }, { indice: "201", imagen: img201 }, { indice: "202", imagen: img202 }, { indice: "203", imagen: img203 }, { indice: "204", imagen: img204 }, { indice: "210", imagen: img210 }, { indice: "211", imagen: img211 }, { indice: "212", imagen: img212 }, { indice: "213", imagen: img213 }, { indice: "214", imagen: img214 }, { indice: "220", imagen: img220 }, { indice: "221", imagen: img221 }, { indice: "222", imagen: img222 }, { indice: "223", imagen: img223 }, { indice: "224", imagen: img224 }, { indice: "230", imagen: img230 }, { indice: "231", imagen: img231 }, { indice: "232", imagen: img232 }, { indice: "233", imagen: img233 }, { indice: "234", imagen: img234 }, { indice: "240", imagen: img240 }, { indice: "241", imagen: img241 }, { indice: "242", imagen: img242 }, { indice: "243", imagen: img243 }, { indice: "244", imagen: img244 }, { indice: "300", imagen: img300 }, { indice: "301", imagen: img301 }, { indice: "302", imagen: img302 }, { indice: "303", imagen: img303 }, { indice: "304", imagen: img304 }, { indice: "310", imagen: img310 }, { indice: "311", imagen: img311 }, { indice: "312", imagen: img312 }, { indice: "313", imagen: img313 }, { indice: "314", imagen: img314 }, { indice: "320", imagen: img320 }, { indice: "321", imagen: img321 }, { indice: "322", imagen: img322 }, { indice: "323", imagen: img323 }, { indice: "324", imagen: img324 }, { indice: "330", imagen: img330 }, { indice: "331", imagen: img331 }, { indice: "332", imagen: img332 }, { indice: "333", imagen: img333 }, { indice: "334", imagen: img334 }, { indice: "340", imagen: img340 }, { indice: "341", imagen: img341 }, { indice: "342", imagen: img342 }, { indice: "343", imagen: img343 }, { indice: "344", imagen: img344 }, { indice: "400", imagen: img400 }, { indice: "401", imagen: img401 }, { indice: "402", imagen: img402 }, { indice: "403", imagen: img403 }, { indice: "404", imagen: img404 }, { indice: "410", imagen: img410 }, { indice: "411", imagen: img411 }, { indice: "412", imagen: img412 }, { indice: "413", imagen: img413 }, { indice: "414", imagen: img414 }, { indice: "420", imagen: img420 }, { indice: "421", imagen: img421 }, { indice: "422", imagen: img422 }, { indice: "423", imagen: img423 }, { indice: "424", imagen: img424 }, { indice: "430", imagen: img430 }, { indice: "431", imagen: img431 }, { indice: "432", imagen: img432 }, { indice: "433", imagen: img433 }, { indice: "434", imagen: img434 }, { indice: "440", imagen: img440 }, { indice: "441", imagen: img441 }, { indice: "442", imagen: img442 }, { indice: "443", imagen: img443 }, { indice: "444", imagen: img444 }]
+  quadrille = createQuadrille(160, image)
 }
 
 function draw() {
-    background(255);
-    video.loadPixels();
-    for (var y = 0; y < video.height; y++) {
-        for (var x = 0; x < video.width; x++) {
+  for (i = 0; i < quadrille.width; i++){
+    for (j = 0; j < quadrille.height; j++){
+      color = quadrille.read(i,j);
 
-            var index = (video.width - x + 1 + (y * video.width)) * 4;
-            var r = video.pixels[index + 0];
-            var g = video.pixels[index + 1];
-            var b = video.pixels[index + 2];
+      r = mapColor(color[0]);
+      g = mapColor(color[1]);
+      b = mapColor(color[2]);
 
-            if (isNaN(r)) r = 0;
-            if (isNaN(g)) g = 0;
-            if (isNaN(b)) b = 0;
-
-            r = mapColor(r);
-            g = mapColor(g);
-            b = mapColor(b);
-
-            var indice = r.toString() + g.toString() + b.toString();
-            var imagen;
-            arregloImagenes.forEach((posibleImagen) => {
-                if (posibleImagen.indice == indice) {
-                    imagen = posibleImagen.imagen;
-                }
-            });
-
-            noStroke();
-            fill(255);
-            rectMode(CENTER);
-            image(imagen, x * vScale, y * vScale, vScale, vScale);
+      var indice = r.toString() + g.toString() + b.toString();
+      var imagen;
+      arregloImagenes.forEach((posibleImagen) => {
+        if (posibleImagen.indice == indice) {
+          imagen = posibleImagen.imagen;
         }
+      });
+
+      quadrille.fill(i, j, imagen)
     }
-    console.log(frameRate())
+  }
+
+  drawQuadrille(quadrille, 0, 0, 40/10, 1.6/3, "grey");
 }
